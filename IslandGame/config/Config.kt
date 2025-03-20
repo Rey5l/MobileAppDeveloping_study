ada789dd5e68bd1a32a97442e8caf2ef64c89d5c
Rasul <119613738+Rey5l@users.noreply.github.com>
2025-03-20 11:01:41 +0300

    Create Config.kt

---
+++ IslandGame/config/Config.kt
@@ -0,0 +1,12 @@
+package config
+
+object Config {
+    const val WIDTH = 20
+    const val HEIGHT = 20
+    const val SIMULATION_TICK = 2000L
+    const val REPRODUCTION_CHANCE = 30
+    const val MAX_ANIMALS_PER_LOCATION = 10
+    const val INITIAL_ANIMALS = 200
+    const val MAX_PLANTS_PER_LOCATION = 3
+    const val MAX_TICKS = 10
+}
